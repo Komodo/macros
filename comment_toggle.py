e056a4b3c17eefed54f4874fab0c3e6da09b988e
Todd Whiteman <toddw@activestate.com>
2014-09-03 12:23:20 -0700

    update comment toggle macro

--- comment_toggle.py
+++ comment_toggle.py
@@ -28,55 +28,58 @@ except KeyError:
     customComment = None
 print('customComment: %r' % (customComment, ))
 
-def toggleLines(lineStart, lineEnd):
-    for lineNo in range(lStart, lEnd+1):
-        log.info("toggling line %d", lineNo)
-        _, line = editor.getLine(lineNo)
-        if (line.strip() == ''):
-            continue
-        # Get start position for text.
-        lineStartPos = editor.positionFromLine(lineNo)
-        wordStartPos = lineStartPos
-        if line.lstrip() != line:
-            wordStartPos = editor.wordEndPosition(lineStartPos, False)
-        if customComment:
-            log.info("using custom comment %r", customComment)
-            word = editor.getTextRange(wordStartPos, wordStartPos+len(customComment))
-            if (word == customComment):
-                editor.targetStart = wordStartPos
-                editor.targetEnd = wordStartPos + len(customComment)
-                editor.replaceTarget('')
-            else:
-                editor.insertText(wordStartPos, customComment)
+def toggleLine(lineNo):
+    log.info("toggling line %d", lineNo)
+    _, line = editor.getLine(lineNo)
+    if (line.strip() == ''):
+        return
+    # Get start position for text.
+    lineStartPos = editor.positionFromLine(lineNo)
+    wordStartPos = lineStartPos
+    if line.lstrip() != line:
+        wordStartPos = editor.wordEndPosition(lineStartPos, False)
+    if customComment:
+        log.info("using custom comment %r", customComment)
+        word = editor.getTextRange(wordStartPos, wordStartPos+len(customComment))
+        if (word == customComment):
+            editor.targetStart = wordStartPos
+            editor.targetEnd = wordStartPos + len(customComment)
+            editor.replaceTarget('')
         else:
-            log.info("using the language's comment service")
-            commenter = view.languageObj.getLanguageService(Ci.koICommenterLanguageService)
-            if not commenter:
-                log.warn("No comment service available for this language")
-                return
-    
-            style = editor.getStyleAt(wordStartPos)
-            log.info("char is: %s", editor.getCharAt(wordStartPos))
-            if style in view.languageObj.getCommentStyles():
-                # It's a comment, uncomment it
-                commenter.uncomment(editor)
-            else:
-                commenter.comment(editor)
-            # Note: The comment service works over the complete selection.
+            editor.insertText(wordStartPos, customComment)
+    else:
+        log.info("using the language's comment service")
+        commenter = view.languageObj.getLanguageService(Ci.koICommenterLanguageService)
+        if not commenter:
+            log.warn("No comment service available for this language")
             return
+
+        style = editor.getStyleAt(wordStartPos)
+        commentStyles = view.languageObj.getCommentStyles()
+        log.info("style is %d, char is: %s, comment styles: %r",
+                 style, chr(editor.getCharAt(wordStartPos)), commentStyles)
+        # Move to the line in question.
+        editor.currentPos = editor.anchor = wordStartPos
+        if style in commentStyles:
+            # It's a comment, uncomment it
+            commenter.uncomment(editor)
+        else:
+            commenter.comment(editor)
 
 view.setFocus()
 editor.beginUndoAction()
 try:
-    # Determine the line range.
     start = editor.selectionStart
     end = editor.selectionEnd
+    # Remove any selection
+    editor.currentPos = editor.anchor = start
     if start != end:
         end = editor.positionBefore(end)
     lStart = editor.lineFromPosition(start)
     lEnd = editor.lineFromPosition(end)
     log.info("iterating over lines %d-%s", lStart, lEnd)
-    toggleLines(lStart, lEnd)
+    for lineNo in range(lStart, lEnd+1):
+        toggleLine(lineNo)
     if start != end:
         editor.selectionStart = editor.positionFromLine(lStart)
         editor.selectionEnd = editor.getLineEndPosition(lEnd)
